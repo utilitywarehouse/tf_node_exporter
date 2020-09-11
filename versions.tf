0509cc7bd5c46824207f9cbb77e6808a47bb97c4
George Angel <github-7574494a@infosecproject.org>
2020-09-11 13:55:58 +0100

    Upgrade module to 0.13 terraform

--- versions.tf
+++ versions.tf
@@ -1,3 +1,11 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    ignition = {
+      source = "terraform-providers/ignition"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+  }
 }
