e01f39be41328ae89903cd2f27c9907b13d8479a
George Angel <github-7574494a@infosecproject.org>
2021-08-09 10:18:48 +0100

    Upgrade to latest node-exporter version

--- variables.tf
+++ variables.tf
@@ -22,5 +22,5 @@ variable "node_exporter_image_url" {
 
 variable "node_exporter_image_tag" {
   description = "The version of the node_exporter image to use."
-  default     = "v1.0.1"
+  default     = "v1.2.2"
 }
